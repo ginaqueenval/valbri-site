9c24fd535d938514b3095de942bda139b1fdcc11
ginaqueenval <akhalvat560@gmail.com>
2026-01-31 02:46:36 +0000

    Fix router basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/valbri-site">
       <App />
     </BrowserRouter>
   </React.StrictMode>
